7c0bd488b42b301e6ff026f666066e875e71bbc8
nh.lee <nh.lee@4cgate.com>
2024-08-14 18:57:40 +0900

    vitest 추가 및 테스트 화면 설정

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -117,7 +117,7 @@ export default tseslint.config({
       // 현재 React 버전을 명시합니다.
       // 명시하지 않을 경우(기본값 'detect') React 라이브러리 전체를 불러오므로
       // 린트 과정에서 속도가 느려질 수 있습니다.
-      ersion: '18.3',
+      version: '18.3',
     },
   },
 });
---
+++ frontend/src/__test__/basic.test.tsx
@@ -0,0 +1,21 @@
+import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { userEvent } from '@testing-library/user-event';
+import Link from '../components/Link';
+
+test('Link changes the state when hovered', async () => {
+  render(<Link page="http://antfu.me">Anthony Fu</Link>);
+
+  const link = screen.getByText('Anthony Fu');
+
+  expect(link).toHaveAccessibleName('Link is normal');
+
+  await userEvent.hover(link);
+
+  await expect.poll(() => link).toHaveAccessibleName('Link is hovered');
+
+  await userEvent.unhover(link);
+
+  await expect.poll(() => link).toHaveAccessibleName('Link is normal');
+});
