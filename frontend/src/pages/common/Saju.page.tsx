fc0a54604038e4563130721226140c36f93d7e1f
Ina <ina9377@gmail.com>
2024-08-26 22:54:29 +0900

    사주 결과보기, 돌아가기 기능 추가

--- frontend/src/pages/common/Saju.page.tsx
+++ frontend/src/pages/common/Saju.page.tsx
@@ -1,5 +1,21 @@
 import axios from 'axios';
 import { useState } from 'react';
+interface SajuItem {
+  key: string;
+  value: {
+    title: string;
+    pre: {
+      first: { title: string; type: string };
+      second: { title: string; type: string };
+    };
+    description: string;
+  };
+}
+interface SajuAnalysis {
+  yinYang: string;
+  fiveElements: string;
+  sajuAnalysis: SajuItem[];
+}
 
 const SajuPage: React.FC = () => {
   const [values, setValues] = useState({
@@ -7,6 +23,7 @@ const SajuPage: React.FC = () => {
     birth: '',
     birthTime: '',
   });
+  const [result, setResult] = useState<SajuAnalysis | null>(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
@@ -14,14 +31,41 @@ const SajuPage: React.FC = () => {
   };
 
   const handleGetTaro = () => {
+    const { name, birth, birthTime } = values;
+    if (!name || !birth || !birthTime) {
+      console.warn('모든 필드를 채워주세요.');
+      alert('모든 필드를 채워주세요.');
+      return;
+    }
+
     console.log(values);
-    axios.post('http://localhost:3000/saju', values).then((data) => console.log(data));
+    axios
+      .post('http://localhost:3000/saju', values)
+      .then((response) => {
+        console.log(response);
+        if (!response?.data) {
+          throw new Error('axios error');
+        }
+        const { code, message, data } = response.data;
+        if (code === 'OK') {
+          const orderData = [
+            { key: 'time', value: data.sajuAnalysis.time },
+            { key: 'day', value: data.sajuAnalysis.day },
+            { key: 'month', value: data.sajuAnalysis.month },
+            { key: 'year', value: data.sajuAnalysis.year },
+          ];
+          setResult({ ...data, sajuAnalysis: orderData });
+        } else {
+          throw new Error(message);
+        }
+      })
+      .catch((error) => console.error(error));
 
     //  transition효과로 생년월일은 줄어들고 결과 화면이 표시된다
   };
-
+  console.log(result);
   return (
-    <main>
+    <main className="SajuPage">
       사주페이지
       <section>
         <form>
@@ -54,6 +98,58 @@ const SajuPage: React.FC = () => {
           </button>
         </form>
       </section>
+      {result && (
+        <section>
+          <h2>나의 오행</h2>
+          <article>
+            <p>{result?.yinYang}</p>
+            <p>{result?.fiveElements}</p>
+          </article>
+          <table>
+            <thead>
+              <tr>
+                <th>사주</th>
+                <th>일주</th>
+                <th>월주</th>
+                <th>년주</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                {result?.sajuAnalysis?.map((item) => (
+                  <td key={item.key}>
+                    <p>{item.value.pre.first.title}</p>
+                    <em>{item.value.pre.first.type}</em>
+                  </td>
+                ))}
+              </tr>
+              <tr>
+                {result?.sajuAnalysis?.map((item) => (
+                  <td key={item.key}>
+                    <p> {item.value.pre.second.title}</p>
+                    <em>{item.value.pre.second.type}</em>
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+          <article>
+            {result?.sajuAnalysis?.map((item) => (
+              <p key={item.key}>{item.value.description}</p>
+            ))}
+          </article>
+        </section>
+      )}
+      <footer>
+        <button
+          type="button"
+          onClick={() => {
+            window.history.back();
+          }}
+        >
+          돌아가기
+        </button>
+      </footer>
     </main>
   );
 };
