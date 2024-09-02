5411994272d4ec38c589c3efaa5494d320275e12
Ina <ina9377@gmail.com>
2024-09-02 22:53:47 +0900

    vite config 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './',
   build: {
-    outDir: 'frontend/dist',
+    outDir: './dist',
     rollupOptions: {
       input: {
-        main: './frontend/index.html',
+        main: './index.html',
       },
     },
   },
