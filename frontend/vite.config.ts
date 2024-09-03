faefe743bb3aa4f726018218e957871bb159a4e7
Ina <ina9377@gmail.com>
2024-09-03 21:09:33 +0900

    index.html 위치 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     outDir: './dist',
     rollupOptions: {
       input: {
-        main: './public/index.html',
+        main: './index.html',
       },
     },
   },
