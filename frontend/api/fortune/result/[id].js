99a1318646496345854ad94ffa5d749d93c128f4
Ina <ina9377@gmail.com>
2024-09-13 07:59:37 +0900

    동적라우팅 설정 수정

--- frontend/api/fortune/result/[id].js
+++ frontend/api/fortune/result/[id].js
@@ -3,6 +3,7 @@ import { handleGetFortuneRequest } from '../../_tools.js';
 export default async function handler(req, res) {
   try {
     const { id } = req.query; // 동적 경로 파라미터는 req.query에서 가져옵니다.
+
     await handleGetFortuneRequest(id, res);
   } catch (error) {
     console.error(error);
